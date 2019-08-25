f69d1a27fb1b4f7efa3d6475b94e58d2da44024a
A210968 <alishia.singh@standardbank.co.za>
2019-08-25 21:59:08 +0200

    fixed error

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -5,15 +5,16 @@ public class BeerSong {
         String word = "bottles";
 
         while (beerNum > 0) {
-            if (beerNum == 1) {
-                word = "bottle";
-            }
-
+ 
             System.out.println(beerNum + " " + word + " of beer on the wall");
             System.out.println(beerNum + " " + word + " of beer.");
             System.out.println("Take one down.");
             System.out.println("Pass it around. ");
             beerNum = beerNum - 1;
+
+            if (beerNum == 1) {
+                word = "bottle";
+            }
             if (beerNum > 0) {
                 System.out.println(beerNum + " " + word + " of beer on the wall ");
             } else {
