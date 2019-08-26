9c87c401bf2887f474614c30126a45c0d18534fb
A210968 <alishia.singh@standardbank.co.za>
2019-08-26 09:32:29 +0200

    changed variable name

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,22 +1,22 @@
 public class BeerSong {
 
     public static void main(String[] args) {
-        int beerNum = 99;
+        int numberOfBottles = 99;
         String word = "bottles";
 
-        while (beerNum > 0) {
+        while (numberOfBottles > 0) {
  
-            System.out.println(beerNum + " " + word + " of beer on the wall");
-            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println(numberOfBottles + " " + word + " of beer on the wall");
+            System.out.println(numberOfBottles + " " + word + " of beer.");
             System.out.println("Take one down.");
             System.out.println("Pass it around. ");
-            beerNum = beerNum - 1;
+            numberOfBottles = numberOfBottles - 1;
 
-            if (beerNum == 1) {
+            if (numberOfBottles == 1) {
                 word = "bottle";
             }
-            if (beerNum > 0) {
-                System.out.println(beerNum + " " + word + " of beer on the wall ");
+            if (numberOfBottles > 0) {
+                System.out.println(numberOfBottles + " " + word + " of beer on the wall ");
             } else {
                 System.out.println("No more bottles of beer on the wall ");
             }
